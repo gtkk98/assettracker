166c960f411a26ac1697185f53300c6917abffa8
Thilina Kumarasiri <119777366+thilina25@users.noreply.github.com>
2026-02-24 01:06:30 +0530

    Initial commit

---
+++ assettracker/src/test/java/com/thilina/assettracker/AssettrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thilina.assettracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssettrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
